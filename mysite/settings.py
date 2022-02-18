89545acc59a54b0e0cb1bad7e659b2105e96762b
imanbayati <bayati.pro@gmail.com>
2022-02-18 16:05:13 +0330

    generate static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR / 'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "statics"
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT=BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
