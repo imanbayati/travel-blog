ca50371746b15abfffbef77330a72771514d05f8
imanbayati <bayati.pro@gmail.com>
2022-02-27 00:53:06 +0330

    created tables contact and post
    created simple tables contact and post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length = 220) 
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    counted_view = models.IntegerField(default=0)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
