391da33b7e27046dba9af310ce918bc781238795
imanbayati <bayati.pro@gmail.com>
2022-03-02 15:51:40 +0330

    meta
    add meta model

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+class ContantAdmin(admin.ModelAdmin):
+    search_fields = ('subject', 'name')
+    filter_fields = ('email',)
+    list_display = ('name','subject','email')
+    date_hierarchy = 'created_date'
+
+admin.site.register(Contact,ContantAdmin)
--- website/models.py
+++ website/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    
     name = models.CharField(max_length=20)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
     massage = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.subject
