e5bfceb597aa9b24a3976b166c310b8d735f085f
imanbayati <bayati.pro@gmail.com>
2022-03-16 11:56:41 +0330

    popular posts
    show popular posts in blog page whit template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,3 +9,14 @@ register = template.Library()
 def countpost():
     posts = Post.objects.filter(status=1)
     return posts
+
+@register.filter
+
+def snippets(value):
+    return value[:200] +' ' + '. . .'
+
+@register.inclusion_tag('blog/blog-populer-post.html')
+
+def popular():
+    posts = Post.objects.filter(status=1).order_by('published_date')
+    return {'posts':posts}
