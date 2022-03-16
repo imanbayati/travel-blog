d77321121ab14d26f169ab2e2a5b7358deb916e9
imanbayati <bayati.pro@gmail.com>
2022-03-16 14:55:20 +0330

    show category widgets
    show category widgets in blog pages whit custom template tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from blog.models import Post
+from blog.models import Post,Category
 
 register = template.Library()
 
@@ -15,8 +15,19 @@ def countpost():
 def snippets(value):
     return value[:200] +' ' + '. . .'
 
+
 @register.inclusion_tag('blog/blog-populer-post.html')
 
 def popular():
-    posts = Post.objects.filter(status=1).order_by('published_date')
+    posts = Post.objects.filter(status=1).order_by('published_date')[:4]
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-posts-category.html')
+
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return{'categories':cat_dict}
