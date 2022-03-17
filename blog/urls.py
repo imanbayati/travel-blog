c9fabab410d8704d0577e30dd28c5f03f990ce6f
imanbayati <bayati.pro@gmail.com>
2022-03-17 13:48:33 +0330

    filter by category
    filter posts by category and show in blog home page

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from blog.views import *
 app_name = 'blog'
 urlpatterns = [
-
     path('',Blog_index,name='index'),
     path('<int:pid>',Blog_single,name='single'),
-    
+    path('category/<str:cat_name>',Blog_index,name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 
 # Create your views here.
-def Blog_index(request):
+def Blog_index(request,cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
@@ -14,3 +16,8 @@ def Blog_single(request,pid):
     context = {'post':post}
     return render(request,'blog/blog-single.html',context)
 
+#def Blog_category(request,cat_name):
+    #posts = Post.objects.filter(status=1)
+    #posts = posts.filter(category__name=cat_name)
+    #context = {'posts': posts}
+    #return render(request,'blog/blog-home.html',context)
